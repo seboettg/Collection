9e8cb1e3066bd7673c6509d883e428aa3a1778d1
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2016-08-17 15:03:39 +0200

    sorting tests, cleanup

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,3 @@
 <?php
 
-include 'vendor/autoload.php';
+require 'vendor/autoload.php';
--- tests/Seboettg/Collection/Test/ArrayListTest.php
+++ tests/Seboettg/Collection/Test/ArrayListTest.php
@@ -1,28 +1,12 @@
 <?php
+
 /*
- * This file is a part of HDS (HeBIS Discovery System). HDS is an 
- * extension of the open source library search engine VuFind, that 
- * allows users to search and browse beyond resources. More 
- * Information about VuFind you will find on http://www.vufind.org
- * 
- * Copyright (C) 2016 
- * HeBIS Verbundzentrale des HeBIS-Verbundes 
- * Goethe-Universität Frankfurt / Goethe University of Frankfurt
- * http://www.hebis.de
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Copyright (C) 2016 Sebastian Böttger <seboettg@gmail.com>
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * You should have received a copy of the MIT license with
+ * this file. If not, please visit: https://opensource.org/licenses/mit-license.php
  */
 
 namespace Seboettg\Collection\Test;
@@ -233,7 +217,6 @@ class ArrayListTest extends \PHPUnit_Framework_TestCase
     }
 
 }
-
 
 class Element implements Comparable
 {

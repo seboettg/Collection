41f4d561e8fb58412eec0af75e36d4efedeacaff
Sebastian Böttger <seboettg@users.noreply.github.com>
2023-12-07 21:35:07 +0100

    Update Comparable.php
    removed unnecessary comments

--- src/Comparable/Comparable.php
+++ src/Comparable/Comparable.php
@@ -16,9 +16,6 @@ namespace Seboettg\Collection\Comparable;
  *
  * This interface imposes a total ordering on the objects of each class that implements it. This ordering is referred
  * to as the class's natural ordering, and the class's compareTo method is referred to as its natural comparison method.
- *
- * @package Seboettg\Collection
- * @author Sebastian Böttger <seboettg@gmail.com>
  */
 interface Comparable
 {

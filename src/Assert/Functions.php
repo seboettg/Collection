8af0a6feff7282867f107b0b88546760911a88c1
Sebastian Böttger <seboettg@users.noreply.github.com>
2023-12-07 19:02:39 +0100

    Update Functions.php
    only made public methods final

--- src/Assert/Functions.php
+++ src/Assert/Functions.php
@@ -52,7 +52,7 @@ final class Functions
         return is_object($value) && method_exists($value, "__toString");
     }
 
-    public static function assertValidCallable(callable $callable, array $parameters)
+    public static final function assertValidCallable(callable $callable, array $parameters)
     {
         $reflected = new ReflectionFunction($callable);
         if (count($reflected->getParameters()) !== count($parameters)) {
@@ -80,7 +80,7 @@ final class Functions
         }
     }
 
-    private static function throwNotApplicableCallableException($paramNumber, $expectedType, $actualType)
+    private static final function throwNotApplicableCallableException($paramNumber, $expectedType, $actualType)
     {
         throw new NotApplicableCallableException(
             sprintf(
@@ -97,7 +97,7 @@ final class Functions
      * @param string $message
      * @return void
      */
-    public static function assertComparable($object, string $message): void {
+    public static final function assertComparable($object, string $message): void {
         if (is_scalar($object)) {
             return;
         }
